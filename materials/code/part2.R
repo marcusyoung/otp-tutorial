2db0d0235fba68640e916f711dfe34d32fac653c
Marcus Young <m.a.young@soton.ac.uk>
2019-02-14 16:47:13 +0000

    Update example code

--- materials/code/part2.R
+++ materials/code/part2.R
@@ -8,7 +8,7 @@ airport_current <- GET(
     arriveBy = TRUE,
     mode = "WALK,TRANSIT", # modes we want the route planner to use
     date = "11-12-2018",
-    time= "09:00am",
+    time= "08:00am",
     maxWalkDistance = 1600, # in metres
     walkReluctance = 5,
     minTransferTime = 600, # in secs (allow 10 minutes)
