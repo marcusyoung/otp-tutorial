4c3b7839c2da1fa014ceeed1c6935e3c5564adf8
Marcus Young <marcus@graspit.co.uk>
2019-03-27 18:11:56 +0000

    Updated to use otpr package in examples

--- materials/code/part3.R
+++ materials/code/part3.R
@@ -7,38 +7,63 @@ library(progress)
 # Import LSO centroids CSV file
 gm_lsoa_centroids <- read.csv("materials/data/gm-lsoa-centroids.csv", stringsAsFactors = FALSE)
 
-# Load the OTP API functions
-source("materials/apps/otp-api-fn.R")
+# Install devtools package first
+install.packages("devtools")
+
+# Now install the otpr package from GitHub
+devtools::install_github("marcusyoung/otpr")
+
+# Now load the package
+library(otpr)
 
 # Call otpConnect() to define a connection called otpcon
 otpcon <-
-  otpConnect(
+  otp_connect(
     hostname = "localhost",
     router = "current",
-    port = "8080",
-    ssl = "false"
+    port = 8080,
+    ssl = FALSE
   )
 
+# As most of the default values of the function arguments are fine for us, we
+# could also use:
+# otpcon <- otp_connect(router = "current")
+
+# Call otp_get_times to get attributes of an itinerary
+otp_get_times(
+  otpcon,
+  fromPlace = c(53.43329,-2.13357),
+  toPlace =   c(53.36274,-2.27293),
+  mode = 'TRANSIT',
+  detail = TRUE,
+  date = '11-25-2018',
+  time = '08:00:00',
+  maxWalkDistance = 1600,
+  walkReluctance = 5,
+  minTransferTime = 600
+)
+
+install.packages("progress")
+library(progress)
 
 total <- nrow(gm_lsoa_centroids) # set number of records
-pb <- progress_bar$new(total = total, format = "(:spin) [:bar] :percent") #progress bar
+pb <- progress_bar$new(total = total, format = "(:spin)[:bar]:percent") #progress bar
 
-# Begin the for loop  
+# Begin the loop  
 for (i in 1:total) {
   pb$tick()   # update progress bar
-  
   response <-
-    otpTripTime(
+    otp_get_times(
       otpcon,
-      from = gm_lsoa_centroids[i, ]$latlong,
-      to = '53.36274,-2.27293',
-      modes = 'WALK,TRANSIT',
+      fromPlace = c(gm_lsoa_centroids[i, ]$latitude, gm_lsoa_centroids[i, ]$longitude),
+      toPlace =  c(53.36274,-2.27293),
+      mode = 'TRANSIT',
       detail = TRUE,
-      date = '2018-11-12',
-      time = '08:00am',
-      maxWalkDistance = "1600", # allows 800m at both ends of journey
-      walkReluctance = "5",
-      minTransferTime = "600"
+      date = '11-26-2018',
+      time = '08:00:00',
+      maxWalkDistance = 1600, # allows 800m at both ends of journey
+      walkReluctance = 5,
+      minTransferTime = 600
     )
   # If response is OK update dataframe
   if (response$errorId == "OK") {
