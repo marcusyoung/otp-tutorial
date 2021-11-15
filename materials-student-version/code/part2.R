65b135192a1b6034b4173a461386d50071c01766
Marcus Young <m.a.young@soton.ac.uk>
2021-11-15 00:36:06 +0000

    Update student version

---
+++ materials-student-version/code/part2.R
@@ -0,0 +1,30 @@
+# Accompanying code for Part 2 of OTP Tutorial
+
+library(httr)
+
+airport_current <- GET(
+  "http://localhost:8080/otp/routers/current/isochrone",
+  query = list(
+    toPlace = "53.3627432,-2.2729342", # latlong of Manchester Airport
+    fromPlace = "53.3627432,-2.2729342", # latlong of Manchester Airport
+    arriveBy = TRUE,
+    mode = "WALK,TRANSIT", # modes we want the route planner to use
+    date = "01-19-2021",
+    time= "08:00am",
+    maxWalkDistance = 1600, # in metres
+    walkReluctance = 5,
+    minTransferTime = 600, # in secs (allow 10 minutes)
+    cutoffSec = 900,
+    cutoffSec = 1800,
+    cutoffSec = 2700,
+    cutoffSec = 3600,
+    cutoffSec = 4500,
+    cutoffSec = 5400
+  )
+)
+
+# convert airport_current to text
+airport_current <- content(airport_current, as = "text", encoding = "UTF-8")
+
+# change file path if required
+write(airport_current, file = "airport-current.geojson")
---
+++ materials-student-version/code/part3.R
@@ -0,0 +1,75 @@
+# Accompanying code for Part 3 of OTP Tutorial
+
+# install and load progress bar package
+install.packages("progress")
+library(progress)
+
+# Import LSO centroids CSV file
+gm_lsoa_centroids <- read.csv("materials/data/gm-lsoa-centroids.csv", stringsAsFactors = FALSE)
+
+# Install the otpr package
+install.packages("otpr")
+
+# Now load the package
+library(otpr)
+
+# Call otpConnect() to define a connection called otpcon
+otpcon <-
+  otp_connect(
+    hostname = "localhost",
+    router = "current",
+    port = 8080,
+    ssl = FALSE
+  )
+
+# As most of the default values of the function arguments are fine for us, we
+# could also use:
+# otpcon <- otp_connect(router = "current")
+
+# Call otp_get_times to get attributes of an itinerary
+otp_get_times(
+  otpcon,
+  fromPlace = c(53.43329,-2.13357),
+  toPlace =   c(53.36274,-2.27293),
+  mode = 'TRANSIT',
+  detail = TRUE,
+  date = '01-19-2021',
+  time = '08:00:00',
+  maxWalkDistance = 1600, # allows nominal 800m at both ends of journey
+  walkReluctance = 5,
+  minTransferTime = 600 # allows at least 10 minutes (600 seconds) for transfers
+)
+
+total <- nrow(gm_lsoa_centroids) # set number of records
+pb <- progress_bar$new(total = total, format = "(:spin)[:bar]:percent") #progress bar
+
+# Begin the loop  
+for (i in 1:total) {
+  pb$tick()   # update progress bar
+  response <-
+    otp_get_times(
+      otpcon,
+      fromPlace = c(gm_lsoa_centroids[i, ]$latitude, gm_lsoa_centroids[i, ]$longitude),
+      toPlace =  c(53.36274,-2.27293),
+      mode = 'TRANSIT',
+      detail = TRUE,
+      date = '01-19-2021',
+      time = '08:00:00',
+      maxWalkDistance = 1600,
+      walkReluctance = 5,
+      minTransferTime = 600
+    )
+  # If response is OK update dataframe
+  if (response$errorId == "OK") {
+    gm_lsoa_centroids[i, "status"] <- response$errorId
+    gm_lsoa_centroids[i, "duration"] <- response$itineraries$duration
+    gm_lsoa_centroids[i, "waitingtime"] <- response$itineraries$waitingTime
+    gm_lsoa_centroids[i, "transfers"] <-response$itineraries$transfers
+  } else {
+    # record error
+    gm_lsoa_centroids[i, "status"] <- response$errorId
+  }
+}
+
+# Export gm_lsoa_centroids df
+write.csv(gm_lsoa_centroids, file="materials/gm_lsoa_centroids.csv")
